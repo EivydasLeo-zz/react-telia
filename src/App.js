e217fda48bde6d533c0c43abf605a0d833d5b8ea
Your Name <you@example.com>
2021-07-19 08:29:23 +0300

    components added to app.js, bootstrap installed.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import Commitment from './components/commitment';
+import HaveServices from './components/haveServices';
+import MobilePlan from './components/mobilePlan';
 
 function App() {
   return (
     <div className="App">
       <main>
         <h1>Hello world</h1>
+        <Commitment />
+        <HaveServices />
+        <MobilePlan />
       </main>
     </div>
   );
---
+++ src/components/mobilePlan.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class MobilePlan extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>MobilePlan Section</h1>
+      </div>
+    );
+  }
+}
+
+export default MobilePlan;
