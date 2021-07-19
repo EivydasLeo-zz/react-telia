b6d3214015f0e37d857146582c332ac6c0ef426a
Your Name <you@example.com>
2021-07-19 08:53:23 +0300

    mobilePlan

--- src/components/mobilePlan.jsx
+++ src/components/mobilePlan.jsx
@@ -4,8 +4,16 @@ class MobilePlan extends Component {
   state = {};
   render() {
     return (
-      <div>
-        <h1>MobilePlan Section</h1>
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the bulk of the card's content.
+          </p>
+          <a href="/" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
       </div>
     );
   }
