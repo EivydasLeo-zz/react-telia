cc9358549f4a2cf91c5a00bcdc743cc128af2b3c
Your Name <you@example.com>
2021-07-19 08:19:01 +0300

    general cleanup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <h1>Hello world</h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;
