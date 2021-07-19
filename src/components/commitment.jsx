e4f26d737e4e08832d6b9ab208761c335fc5e050
Your Name <you@example.com>
2021-07-19 13:35:55 +0300

    mobile plan card css done, commitment css, custom radio

--- src/components/commitment.jsx
+++ src/components/commitment.jsx
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
+import CustomRadio from './common/customRadio';
 
 class Commitment extends Component {
+  handleRadio = () => {
+    console.log('radio was pressed');
+    // atvaizduoti 24men. is CustomRadio komponento vidaus
+
+    // pakeisti i teksta is not slected i selected kai paspaudziam ant mygtuko
+
+    // iskviesti handleRadio funkcija is CustomRadio componento
+  };
+
   state = {};
   render() {
     return (
-      <div>
-        <h1>Commitment Section</h1>
+      <div className="commitment-component">
+        <div className="c__selection">
+          <span className="c__select">
+            <CustomRadio>24 men.</CustomRadio>
+          </span>
+          <span className="c__select-title">24 men</span>
+        </div>
+        <div className="c__selection">
+          <span className="c__select"></span>
+          <span className="c__select-title">Be isipareigojimu</span>
+        </div>
       </div>
     );
   }
--- src/components/mobilePlan.jsx
+++ src/components/mobilePlan.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import ListImage from '../static/check.png';
 class MobilePlan extends Component {
   state = {};
   render() {
@@ -7,13 +7,28 @@ class MobilePlan extends Component {
       <div className="mobile-plan">
         <h6 className="plan__header">Mobilus START 1</h6>
         <h2 className="plan__dataAllow">1 GB</h2>
-        <small className="plan__dataEU">Iš jų 1GB ES/EE</small>
+        <small className="plan__dataEU">Iš jų 1 GB ES/EEE</small>
         <p className="plan__min-sms">Neribotos MIN ir SMS</p>
-        <div className="plan__features">Sarasas</div>
+        <div className="plan__features">
+          <div className="hr"></div>
+          <ul className="features__list">
+            <li>
+              <img className="features__check" src={ListImage} alt="check" />
+              <span>M. parašas (6 mėn.)</span>
+            </li>
+            <li>
+              <img className="features__check" src={ListImage} alt="check" />
+              <span>Įrangos draudimas (3 mėn.)</span>
+            </li>
+          </ul>
+          <div className="hr"></div>
+        </div>
         <div className="plan__bottom">
           <div className="bottom__price-part">
-            <h3>9,00 eur/men</h3>
-            <small>24 mėn. sutartis</small>
+            <h3 className="bottom__price">
+              9,00 <span>&euro;/men</span>
+            </h3>
+            <small className="bottom__term">24 mėn. sutartis</small>
           </div>
           <button className="plan__cta">Domina</button>
         </div>
